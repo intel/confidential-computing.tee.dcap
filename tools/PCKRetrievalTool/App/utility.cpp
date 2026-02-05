intel/SGXDataCenterAttestationPrimitives

Support build time setting of enclave load directory
The current code for loading enclaves (pce, id_enclave, qe3, tdqe, qve)
tries to find the enclave file in the directory of the currently
loaded library (as reported by dladdr), or in the directory of the
current executable (as reported by /proc/self/exe).

Neither of these approaches is sufficiently flexible to work with
all Linux distro filesystem layout policies. In particular distros
may desire to have a specific directory location exclusively for
the shipping of enclaves, separate from any native libraries or
executables.

This introduces support for an "SGX_ENCLAVE_PATH" variable in the
makefiles, which is used to define an SGX_ENCLAVE_PATH symbol in
code.

By default SGX_ENCLAVE_PATH path will get defined to an empty
string at the C level and so current code behaviour will not
be changed.

If this is set though, then it will be used to locate the enclaves,
with no fallback to searching relative to the library or binary.

Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

tools/PCKRetrievalTool/App/utility.cpp
@@ -235,9 +235,9 @@ bool load_enclave(const char* enclave_name, sgx_enclave_id_t* p_eid)
     char enclave_path[MAX_PATH] = "";
 #endif
 
+#if defined(_MSC_VER)
     if (!get_program_path(enclave_path, MAX_PATH - 1))
         return false;
-#if defined(_MSC_VER)    
     if (_tcsnlen(enclave_path, MAX_PATH) + _tcsnlen(enclave_name, MAX_PATH) + sizeof(char) > MAX_PATH)
         return false;
     (void)_tcscat_s(enclave_path, MAX_PATH, enclave_name);
@@ -248,6 +248,16 @@ bool load_enclave(const char* enclave_name, sgx_enclave_id_t* p_eid)
     sgx_create_enclave_func_t p_sgx_create_enclave = (sgx_create_enclave_func_t)FINDFUNCTIONSYM(sgx_urts_handle, "sgx_create_enclavea");
 #endif
 #else
+    if (*SGX_ENCLAVE_PATH) {
+        if ((strlen(SGX_ENCLAVE_PATH) + 1 + 1) > MAX_PATH) {
+            return false;
+        }
+        (void)strcpy(enclave_path, SGX_ENCLAVE_PATH);
+        (void)strcat(enclave_path, "/");
+    } else {
+        if (!get_program_path(enclave_path, MAX_PATH - 1))
+            return false;
+    }
     if (strnlen(enclave_path, MAX_PATH) + strnlen(enclave_name, MAX_PATH) + sizeof(char) > MAX_PATH)
         return false;
     (void)strncat(enclave_path, enclave_name, strnlen(enclave_name, MAX_PATH));

intel/confidential-computing.tee.dcap

tools/PCKRetrievalTool/App/utility.cpp
@@ -214,9 +214,9 @@ bool load_enclave(const char* enclave_name, sgx_enclave_id_t* p_eid)
     char enclave_path[ProgPathBufferSize] = "";
 #endif
 
+#if defined(_MSC_VER)
     if (!get_program_path(enclave_path, ProgPathBufferSize))
         return false;
-#if defined(_MSC_VER)    
     if (_tcsnlen(enclave_path, ProgPathBufferSize) + _tcsnlen(enclave_name, ProgPathBufferSize) + sizeof(char) > ProgPathBufferSize)
         return false;
     (void)_tcscat_s(enclave_path, ProgPathBufferSize, enclave_name);
@@ -227,6 +227,16 @@ bool load_enclave(const char* enclave_name, sgx_enclave_id_t* p_eid)
     sgx_create_enclave_func_t p_sgx_create_enclave = (sgx_create_enclave_func_t)FINDFUNCTIONSYM(sgx_urts_handle, "sgx_create_enclavea");
 #endif
 #else
+    if (*SGX_ENCLAVE_PATH) {
+        if ((strlen(SGX_ENCLAVE_PATH) + 1 + 1) > ProgPathBufferSize) {
+            return false;
+        }
+        (void)strcpy(enclave_path, SGX_ENCLAVE_PATH);
+        (void)strcat(enclave_path, "/");
+    } else {
+        if (!get_program_path(enclave_path, ProgPathBufferSize))
+            return false;
+    }
     if (strnlen(enclave_path, ProgPathBufferSize) + strnlen(enclave_name, ProgPathBufferSize) + sizeof(char) > ProgPathBufferSize)
         return false;
     (void)strncat(enclave_path, enclave_name, strnlen(enclave_name, ProgPathBufferSize));