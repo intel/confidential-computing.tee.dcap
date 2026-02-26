a19c0c7703bc78d8adfb400ee966546aaaac93a8
Jiang, Hongyan <hongyan.jiang@intel.com>
2026-02-26 07:33:18 +0000

    Update the order of tcb_status and platform_tcb_date to make it backward compatible
    Signed-off-by: Jiang, Hongyan <hongyan.jiang@intel.com>

--- QuoteGeneration/quote_wrapper/servtd_attest/inc/servtd_com.h
+++ QuoteGeneration/quote_wrapper/servtd_attest/inc/servtd_com.h
@@ -83,8 +83,8 @@ struct servtd_tdx_quote_suppl_data {
         sgx_prod_id_t              isv_prod_id;          /*   730     2 */
         sgx_isv_svn_t              isv_svn;              /*   732     2 */
         time_t                     tcb_date;             /*   734     64 */ /* Represents the effective TCB date, calculated as: min(platform_tcb_date, qe_tcb_date) */
-        time_t                     platform_tcb_date;    /*   798     64 */
-        char                       tcb_status[TCB_STATUS_LEN];           /*   862     32 */
+        char                       tcb_status[TCB_STATUS_LEN];           /*   798     32 */
+        time_t                     platform_tcb_date;    /*   830     64 */
         time_t                     qe_tcb_date;          /*   894     64 */
         char                       qe_tcb_status[TCB_STATUS_LEN];        /*   958     32 */
 };
